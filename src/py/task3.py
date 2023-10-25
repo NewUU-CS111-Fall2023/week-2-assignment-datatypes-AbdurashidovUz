a30270684f69d7998c75b2d23cbb656b651ffdb4
AbdurashidovUz <127374337+AbdurashidovUz@users.noreply.github.com>
2023-10-25 21:01:21 +0500

    Update task3.py

--- src/py/task3.py
+++ src/py/task3.py
@@ -1,1 +1,16 @@
+import random
+
+def generate_large_number():
+    return random.randint(10**99, 10**100 - 1)
+
+def divide_by_input(number):
+    return large_number // number
+
+number_A = int(input("Enter a number: "))
+
+large_number = generate_large_number()
+
+result = divide_by_input(number_A)
+
+print("The result of dividing the 100-digit number", large_number, " by ", number_A, "is:", result)
 
