a1d79fa3eb0ae1892ef11fffdea3a59abf56082e
AbdurashidovUz <127374337+AbdurashidovUz@users.noreply.github.com>
2023-10-25 20:56:18 +0500

    Update task2.py

--- src/py/task2.py
+++ src/py/task2.py
@@ -1,1 +1,18 @@
+import random
 
+def guessing_game():
+    number_to_guess = random.randint(1, 100)
+    guessed = False
+    
+    while not guessed:
+        guess = int(input("Guess a number between 1 and 100: "))
+        
+        if guess < number_to_guess:
+            print("Too low!")
+        elif guess > number_to_guess:
+            print("Too high!")
+        else:
+            print("Congratulations! You guessed the correct number.")
+            guessed = True
+            
+guessing_game()
